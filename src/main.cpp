a351205e9f99c5417b412f0813a6ea6038be8204
Mehmet Ulrich <mehmetulrich@gmail.com>
2021-03-20 00:06:09 +0100

    prototypes of functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,23 @@
 #define CHIPSET WS2812B
 CRGB leds[NUM_LEDS];
 
+// Prototypes of functions, implementation at the end
+void lights_on();
+// turns the light smoth on
+void lights_off();
+// turns the light smoth off
+
+void setup(){
+    FastLED.addLeds<CHIPSET, 2>(leds, NUM_LEDS);
+}
+
+void loop(){
+    lights_on();
+    delay(1000);
+    lights_off();
+    delay(1000);
+}
+
 void lights_on(){
     for (int i = 0; i <= 255; i ++){
 	FastLED.setBrightness(i);
@@ -20,15 +37,4 @@ void lights_off(){
     	FastLED.show();
 	delay (5);
     }
-}
-
-void setup(){
-    FastLED.addLeds<CHIPSET, 2>(leds, NUM_LEDS);
-}
-
-void loop(){
-    lights_on();
-    delay(1000);
-    lights_off();
-    delay(1000);
 }
